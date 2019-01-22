f5185949070736600efbbe9f6c02e2f47ea10622
brunothelima <brunothelima@gmail.com>
2019-01-22 15:33:15 -0200

    Navigation mode / insert method / Various improves

--- js/Gallery.js
+++ js/Gallery.js
@@ -4,51 +4,67 @@ export class Gallery extends Component {
 
 	constructor(element) {
 		super(element);
+		// Current item on display
+		this._curr = 0;
 		// Creates the gallery__lens wrapper 
 		this._lens = document.createElement('div');
 		// Init the _items prop with the component children
-		this._items = Array.from(this._element.children);
-		this._currItem = 0;
-		// Add the component classes for styling
-		this._element.classList.add('gallery');
-		this._lens.classList.add('gallery__lens');
+		this._items = Array.from(this._element.children)
 		// init component
 		this.init()
 	}
-
-	// Initialize the component
-	init() {
-
-		this.render(); // Render new component HTML
-		
-		// Bind prev button click to prev method
-		this._element.querySelector('.gallery__prev')
-			.addEventListener('click', event => {
-				this.prev();
-			});
-		// Bind pagination button click to goTo method
-		this._element.querySelectorAll('.gallery__index').forEach(index => {
-			index.addEventListener('click', event => {
-				this.goTo(event.target.innerText);
-			});
-		})
-		// Bind next button click to next method
-		this._element.querySelector('.gallery__next')
-			.addEventListener('click', event => {
-				this.next();
-			});
+	// Computed total of items
+	get total() {
+		return this._items.length;
 	}
-	render() {
-		// Transfers the child components to gallery__lens wrapper
-		this._items.map(item => {
-			item.classList.add('gallery__item');
-			return this._lens.appendChild(item);	
-		});	
-		// Append the new gallery_lens element filled with children
-		this._element.appendChild(this._lens);
-		// Add gallery__item--active class to the first gallery__item
-		this._items[this._currItem].classList.add('gallery__item--active')
-		// Render the gallery__controll elements
+	// Hide all _items besides _items[_curr] 
+	set setCurrent (curr=0) {
+		// Active item style class
+		const activeClass = 'gallery__item--active';
+		// Set new value for _curr
+		this._curr = curr;
+		// Remove active class from all items
+		this._items.map(item => item.classList.remove(activeClass));
+		// Add active class to the _items[_curr]
+		this._items[this._curr].classList.add(activeClass);
+	}
+	// Set _curr as last item if first item is shown, set next item index
+	prev() {
+		this.setCurrent = (this._curr <= 0) ? this.total - 1 : this._curr - 1;
+	}
+	// Show selected item[index] on pagination
+	goTo(index) {
+		this.setCurrent = index - 1;
+	}
+	// Set _curr as first item if last item is shown, set next item index
+	next() {
+		this.setCurrent = (this._curr >= (this.total - 1)) ? 0 : this._curr + 1;
+	}
+	// Update _items array and reload the pagination controlls 
+	update() {
+		this._items = Array.from(this._lens.children);
+		// Set current item visible
+		this._items[this._curr].classList.add('gallery__item--active');
+		this.buildPagination();
+	}
+	insert(items) {
+		if (items instanceof HTMLElement) {
+			items = [items];
+		}
+		// Wrapp items in a gallery__item div
+		this._lens.innerHTML += items.map((item, index) => {
+			item.remove();
+			return `<div class="gallery__item">${item.outerHTML}</div>`;
+		}).join('');
+		// Update vars and controlls
+		this.update();
+	}
+	// Render the gallery__controll elements and bind navigation events
+	buildPagination() {
+		if (this._element.querySelector('.gallery__controlls')) {
+			this._element.querySelector('.gallery__controlls').remove();
+		}
+		// Render the gallery__controll HTML template
 		this._element.insertAdjacentHTML('beforeend', `
 			<div class="gallery__controlls">
 				<button class="gallery__prev">Prev</button>
@@ -58,32 +74,23 @@ export class Gallery extends Component {
 				<button class="gallery__next">Next</button>
 			</div>
 		`);
+		// Bind prev button click to prev method
+		this._element.querySelector('.gallery__prev').addEventListener('click', _ => this.prev());
+		// Bind next button click to next method
+		this._element.querySelector('.gallery__next').addEventListener('click', _ => this.next());
+		// Bind pagination button click to goTo method
+		for(let item of this._element.querySelectorAll('.gallery__index')) {
+			item.addEventListener('click', _ => this.goTo(event.target.innerText));
+		}
 	}
-
-	// Hide all _items besides _items[_currItem] 
-	updateItems() {
-		// Active item style class
-		const activeClass = 'gallery__item--active';
-		// Remove active class from all items
-		this._items.map(item => item.classList.remove(activeClass))
-		// Add active class to the _items[_currItem]
-		this._items[this._currItem].classList.add(activeClass)
-	}
-	// Show previews item
-	prev() {
-		// Set _currItem as last item if first item is shown, set next item index
-		this._currItem = (this._currItem <= 0) ? this._items.length - 1 : this._currItem - 1;
-		this.updateItems();
-	}
-	// Show selected item[index] on pagination
-	goTo(index) {
-		this._currItem = index - 1;
-		this.updateItems();
-	}
-	// Show next item
-	next() {
-		// Set _currItem as first item if last item is shown, set next item index
-		this._currItem = (this._currItem >= (this._items.length - 1)) ? 0 : this._currItem + 1;
-		this.updateItems();
+	// Creates a wrapper for every item on the list
+	init() {
+		// Add the component classes for styling
+		this._element.classList.add('gallery');
+		// Append the new gallery_lens element filled with children
+		this._lens.classList.add('gallery__lens');
+		this._element.appendChild(this._lens);
+		// Transfers the child components to gallery__lens wrapper
+		this.insert(this._items);
 	}
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 import {Gallery} from './Gallery.js';
 
 const gallery = new Gallery('#gallery');
+setTimeout(function() {
+	gallery.insert(document.createElement('img'));
+}, 3000);
