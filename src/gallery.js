dd3c793312015332ea2e59eac89a26a7b0c179f6
Bruno Lima <brunothelima@gmail.com>
2019-01-29 01:34:06 -0200

    Overall optiomization

--- src/gallery.js
+++ src/gallery.js
@@ -1,5 +1,27 @@
 /*eslint no-console: ["error", { allow: ["warn", "error"] }] */
-
+/*
+	Validate the existence of the element and
+	returns the element reference
+*/
+function extractElement(target) {
+	try {
+		if (target instanceof HTMLElement) {
+			return target;
+		} else if (typeof target === 'string' || target instanceof String) {
+			// Checks if the element reference exists
+			if (document.querySelector(target)) {
+				return document.querySelector(target);
+			} else {
+				// Throw error if the selector match is undefined
+				throw `The selector '${target}' has no element match.`;
+			}
+		} else {
+			throw 'Invalid initialization selector/element';
+		}
+	} catch(error) {
+		throw new Error(error);
+	}
+}
 /*
 	Gallery Component;
 	Creates an slider of elements with pagination and controlls
@@ -10,38 +32,15 @@ export default class {
 		Inherit $element from Component class
   */
 	constructor(target) {
-		this._curr = 0; // Current visible item index 
-		this._items = []; // Itens to display iterable
-		this._bullets; // Will become gallery__index iterable
-		this.$element = {}; // Target element
-		this.$lens; // Will become gallery__lens element reference
-		this.$controlls; // Will become gallery__controlls element reference
+		this.$element = extractElement(target); // Target element
+		this.$list; // Will become gallery__list element reference
+		this.$ctrls; // Will become gallery__ctrls element reference
 		this.$prev; // Will become gallery__prev element reference
 		this.$next; // Will become gallery__next reference
-		this.init(target);
-	}
-	/*
-		Validate the existence of the element and
-		returns the element reference
-	*/
-	extractElement(target) {
-		try {
-			if (target instanceof HTMLElement) {
-				return target;
-			} else if ((typeof target === 'string' || target instanceof String) && target.match('(#|.).*')) {
-				// Checks if the element reference exists
-				if (document.querySelector(target)) {
-					return document.querySelector(target);
-				} else {
-					// Throw error if the selector match is undefined
-					throw `The selector '${target}' has no element match.`;
-				}
-			} else {
-				throw 'Invalid initialization selector/element';
-			}
-		} catch(error) {
-			throw new Error(error);
-		}
+		this._current = 0; // currentent visible item index 
+		this._buttons; // Will become gallery__index iterable
+		this._items = [...this.$element.children] || []; // Itens to display iterable
+		this.render();
 	}
 	/*
 		Getter for total items entries
@@ -50,24 +49,24 @@ export default class {
 		return this._items.length;
 	}
 	/*
-		Sets the current item to be displayed
-		@curr: Item index
+		Sets the currentent item to be displayed
+		@current: Item index
 	*/
-	set currentIndex (curr=0) {
-		// Set the new value for _curr
-		this._curr = curr;
+	set current (current=0) {
+		// Set the new value for _current
+		this._current = current;
 		/*
 			Remove active class from all items
-			Then adds active class to the current item to be displayed
+			Then adds active class to the currentent item to be displayed
 		*/
 		this._items.map(item => item.style.display = 'none');
-		this._items[this._curr].style.display = 'block';
+		this._items[this._current].style.display = 'block';
 		/* 
 			Remove active class from all bullets
-			Then adds active class to the current bullet to be hightlighted
+			Then adds active class to the currentent bullet to be hightlighted
 		*/
-		this._bullets.map(item => item.disabled = false);
-		this._bullets[this._curr].disabled = true;
+		this._buttons.map(item => item.disabled = false);
+		this._buttons[this._current].disabled = true;
 	}
 	/*
 		Set the previews element to be displayed
@@ -75,17 +74,17 @@ export default class {
 	*/
 	prev() {
 		/*
-			Iterates over _curr
-			Returns last _item index if _curr was the first item availble
+			Iterates over _current
+			Returns last _item index if _current was the first item availble
 		*/
-		this.currentIndex = this._curr <= 0 ? this.total - 1 : this._curr - 1;
+		this.current = this._current <= 0 ? this.total - 1 : this._current - 1;
 	}
 	/*
 		Set the selected element to be displayed
 		Used as callback for the galler__index buttom @click event
 	*/
 	goTo(index) {
-		this.currentIndex = index;
+		this.current = index;
 	}
 	/*
 		Set the next element to be displayed
@@ -93,12 +92,12 @@ export default class {
 	*/
 	next() {
 		/*
-			Iterates over _curr
-			Returns first _item index if _curr was the last item availble
+			Iterates over _current
+			Returns first _item index if _current was the last item availble
 		*/
-		this.currentIndex = this._curr >= (this.total - 1) ? 0 : this._curr + 1;
+		this.current = this._current >= (this.total - 1) ? 0 : this._current + 1;
 	}
-	// Insert new elements to the $lens element
+	// Insert new elements to the $list element
 	insert(elements=[]) {
 		try {
 			// Grants that elements is an arrayh
@@ -106,52 +105,64 @@ export default class {
 			if (!elements.every(element => element instanceof HTMLElement)) {
 				throw 'The arguments passed for insert(elements) must be an HTMLElement object/list';
 			}
-			// Append each element passed as argument to the $lens element
+			// Append each element passed as argument to the $list element
 			for(const element of elements) {
-				this.$lens.appendChild(element);
+				this.$list.appendChild(element);
 			}
 		} catch(error) {
 			throw new Error(error);
 		}
 	}
 	/*
-		Render gallery__lens element into the component;
-		Update $lens reference
+		Render gallery__list element into the component;
+		Update $list reference
 		Update _items elements list
 	*/
-	renderLens() {	
-		// Remove the current rendered gallery__lens element
-		if (this.$lens) this.$lens.remove();
-
-		// Insert gallery__lens html into the component
+	renderList() {	
+		// Insert gallery__list html into the component
 		this.$element.insertAdjacentHTML('afterbegin', `
-			<div class="gallery__lens">
+			<div class="gallery__list">
 				${this._items.map(item => {  
 					/*
-						Iteration over _items, render each item html into gallery__lens
+						Iteration over _items, render each item html into gallery__list
 						Retrive innerHTML for allread rendered elements and outerHTML for new elements
 					*/
-					const html = (item.className.includes('gallery__item')) ? item.innerHTML : item.outerHTML;
+					const html = (item.className.includes('gallery__item')) 
+						? item.innerHTML : item.outerHTML;
 					return `<div class="gallery__item">${html}</div>`;
 				}).join('')}
 			</div>`);
-		// Set _lens reference to the new rendered gallery__lens
-		this.$lens = document.querySelector('.gallery__lens');
-		// Set _items as new gallery__items rendered
-		this._items = [...this.$lens.children];
+		/*
+			Set _List reference to the new rendered gallery__list
+			Set _items as list of new gallery__items rendered
+		*/
+		this.$list = document.querySelector('.gallery__list');
+		this._items = [...this.$list.children];
+		/*
+			Observer object for $list element
+			Update the component html on any update on gallery__list content
+		*/
+		const mo = new MutationObserver((mutations, observer) => {
+			// Disconnects the observer to prevent infinite loop
+			observer.disconnect();
+			// Update _items with new/deleted content
+			this._items = [...this.$list.children];
+			// Re-render the component view
+			this.render(); 
+		});
+		// Initialize $list obervation
+		mo.observe(this.$list, {childList: true});
 	}
 	/*
-		Render gallery__controlls element into the component;
+		Render gallery__ctrls element into the component;
 		Bind events to the controll bullets
-		Update controll elements reference ($controlls, _bullets, $next, $prev)
-		Set observer for any changes on gallery__lens content
+		Update controll elements reference ($ctrls, _buttons, $next, $prev)
+		Set observer for any changes on gallery__list content
 	*/
-	renderControlls() {
-		// Remove the current rendered gallery__controlls element
-		if (this.$controlls) this.$controlls.remove();
-		// Insert gallery__lens html into the component
+	renderCtrls() {
+		// Insert gallery__list html into the component
 		this.$element.insertAdjacentHTML('beforeend', `
-			<div class="gallery__controlls">
+			<div class="gallery__ctrls">
 				<button class="gallery__prev">Prev</button>
 				${this._items.map((item, index) => { // Iteration over _items, renders bullets relative to gallery__index indexes
 					return `<button class="gallery__index">${index+1}</button>`;
@@ -159,76 +170,51 @@ export default class {
 				<button class="gallery__next">Next</button>
 			</div>
 		`);
-		// Set _controlls as reference for the updated gallery__controlls element
-		this.$controlls = document.querySelector('.gallery__controlls');
-		// Extract $controlls buttons as iterable $controlls children.
-		this._bullets = [...this.$controlls.children];
-		// Remove gallery__prev button from _bullets iterable, Set $prev as the its reference
-		this.$prev = this._bullets.shift();
-		// Bind $prev click event to the prev() method
-		this.$prev.addEventListener('click', () => this.prev());
-		// Remove gallery__next button from _bullets iterable, Set $next as the its reference
-		this.$next = this._bullets.pop();
-		// Bind $next click event to the next() method
-		this.$next.addEventListener('click', () => this.next());
+		// Set _controlls as reference for the updated gallery__ctrls element
+		this.$ctrls = document.querySelector('.gallery__ctrls');
+		// Extract $ctrls buttons as iterable $ctrls children.
+		this._buttons = [...this.$ctrls.children];
 		/*
-			Iterates over _bullets;
-			Bind click event for each bullet element
+			Remove gallery__prev button from _buttons iterable, Set $prev as the its reference
+			Bind $prev click event to the prev() method
 		*/
-		for(const [index, item] of this._bullets.entries()) {
+		this.$prev = this._buttons.shift();
+		this.$prev.addEventListener('click', () => this.prev());
+		/*
+			Remove gallery__next button from _buttons iterable, Set $next as the its reference
+			Bind $next click event to the next() method
+		*/
+		this.$next = this._buttons.pop();
+		this.$next.addEventListener('click', () => this.next());
+		// Bind click event for each bullet element
+		for(const [index, item] of this._buttons.entries()) {
 			item.addEventListener('click', () => this.goTo(index));
 		}
 		/*
-			Set _curr as the last active item before the render
-			Reset _curr to the first item if item index is undefined
+			Set _current as the last active item before the render
+			Reset _current to the first item if item index is undefined
 		*/
-		this.currentIndex = (this._curr <= (this.total-1)) ? this._curr : 0;
-		/*
-			Observer object for $lens element
-			Update the component html on any update on gallery__lens content
-		*/
-		const mo = new MutationObserver((mutations, observer) => {
-			// Disconnects the observer to prevent infinite loop
-			observer.disconnect();
-			// Update _items with new/deleted content
-			this._items = [...this.$lens.children];
-			// Re-render the component view
-			this.render(); 
-		});
-		// Initialize $lens obervation
-		mo.observe(this.$lens, {childList: true});
+		this.goTo(0);
 	}
 	/*
 		Updates the component html
 	*/
-	render() {		
+	render() {
 		try {
 			// Checks if there is child elements for the gallery initialization
 			if (!this._items.length) {
-				throw `No child elements found on gallery instace $target.`;
+				throw `<target> element must have at least one children.`;
 			}
-			this.renderLens(); // Render $lens element
-			this.renderControlls(); // Render $controlls element
+			// Clear the target content
+			this.$element.innerHTML = ``;
+			/*
+				Render List wrapper and ctrls.
+				Must be in this order to work properly
+			*/
+			this.renderList(); // Render $list element
+			this.renderCtrls(); // Render $ctrls element
 		} catch(error) {
 			throw new Error(error);
 		}
-	}
-	/*
-		Initializes _items
-		Render $lens and controlls 
-	*/
-	init(target) {
-		// Extracts element target form reference or selector passed as argument o new Gallery()
-		this.$element = this.extractElement(target);
-		// Set _items with the inital child elements of target
-		this._items = [...this.$element.children] || [];
-		// Remove all childs from target after assign then to _items
-		while(this.$element.firstChild) {
-			this.$element.removeChild(this.$element.firstChild);
-		}
-		// Adds style and activation classes to the component
-		// this.$element.classList.add('gallery','gallery--active');
-		// Transfers the child components to gallery__lens wrapper
-		this.render();
 	}
 }
